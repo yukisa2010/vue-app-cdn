177dd5500d54102d5f1218e1b0fabb8b6000f6d8
yukisa2010 <43121712+yukisa2010@users.noreply.github.com>
2019-11-10 20:32:43 +0900

    form-item-making

--- index.js
+++ index.js
@@ -1,22 +1,62 @@
-Vue.component('todo-items', {
+Vue.component('todo-item', {
   template: `
-  <li>
+  <li 
+    v-bind:class="{ 'done': state }"
+  >      
     <input 
       type="checkbox" 
       v-bind:checked="state"
-      v-on:change="checked"
+      v-on:click="clicking($event, id)"
     >
     {{ todo }}
   </li>`,
-  props: 
-    {
-        id: Number,
-        todo: String,
-        state: Boolean
-    },
+  props: {
+      id: Number,
+      todo: String,
+      state: Boolean
+  },
   methods: {
-    checked: function() {
-      
+    clicking: function($event, index) {
+      const newItems = app.items.map(function(el) {
+        if(el.id === index) {
+          el.state = !el.state
+        }
+        return el
+      })
+      app.items = newItems
+    }
+  }
+})
+
+Vue.component('form-item', {
+  template: `
+    <form v-on:submit.prevent>
+      <input type="text" v-bind:value="value" v-on:change="changeText($event)"/>
+      <button v-on:click="addItem">Add</button>    
+    </form>`,
+  props: ['value'],
+  methods: {
+    changeText: function($event, value) {
+      app.newItem = $event.target.value
+    },
+    addItem: function() {
+      if(app.newItem === '') {
+        return
+      }
+      //maxIdを取得
+      let maxId;
+      app.items.forEach(function(e) {
+        if(maxId < e.id) {
+          maxId = e.id
+        }
+      })
+      //maxId + 1でobj作成
+      var newObj = {
+        id: maxId, todo: app.newItem, state: false
+      }
+      //itemsにセット
+      app.items.push(newObj)
+      app.newItem = ''
     }
   }
 })
@@ -28,8 +68,10 @@ var app = new Vue({
         { id:1, todo: 'aaa', state: false },
         { id:2, todo: 'bbb', state: false },
         { id:3, todo: 'ccc', state: false },
-      ]
+      ],
+      newItem: ''
   },
+  })
 
   // data: {
   //     newItem: ''  
@@ -43,24 +85,4 @@ var app = new Vue({
   //   immediate: true
   // },
   // methods: {
-  //   addItem: function() {
-  //     if(this.newItem === '') {
-  //       return
-  //     }
-  //     //maxIdを取得
-  //     let maxId;
-  //     this.items.forEach(function(e) {
-  //       if(maxId < e.id) {
-  //         maxId = e.id
-  //       }
-  //     })
-  //     //maxId + 1でobj作成
-  //     var newObj = {
-  //       id: maxId, todo: this.newItem, state: false
-  //     }
-  //     //itemsにセット
-  //     this.items.push(newObj)
-  //     this.newItem = ''
-  //   }
   // }
-})
