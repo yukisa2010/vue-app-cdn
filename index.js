b110e10430ab48994ebd1ce17c86ce33ac27797a
yukisa2010 <43121712+yukisa2010@users.noreply.github.com>
2019-11-10 17:26:10 +0900

    component revised

--- index.js
+++ index.js
@@ -1,47 +1,66 @@
-
-Vue.component('todo-item',{
-  template: '<li v-bind:class="{ done: done }" v-if="display"><input type="checkbox" v-model="done">{{ todo }}</li>',
-  props: ['todo', 'done', 'display']
+Vue.component('todo-items', {
+  template: `
+  <li>
+    <input 
+      type="checkbox" 
+      v-bind:checked="state"
+      v-on:change="checked"
+    >
+    {{ todo }}
+  </li>`,
+  props: 
+    {
+        id: Number,
+        todo: String,
+        state: Boolean
+    },
+  methods: {
+    checked: function() {
+      
+    }
+  }
 })
-
-
 
 var app = new Vue({
   el: '#app',
-  data: function(){
-    return {
+  data: {
       items: [
         { id:1, todo: 'aaa', state: false },
         { id:2, todo: 'bbb', state: false },
         { id:3, todo: 'ccc', state: false },
-      ],
-      newItem: ''  
-    }
+      ]
   },
-  methods: {
-    addItem: function() {
-      if(this.newItem === '') {
-        return
-      }
-      //maxIdを取得
-      let maxId;
-      this.items.forEach(function(e) {
-        if(maxId < e.id) {
-          maxId = e.id
-        }
-      })
-      //maxId + 1でobj作成
-      var newObj = {
-        id: maxId, todo: this.newItem, state: false
-      }
-      //itemsにセット
-      this.items.push(newObj)
-      this.newItem = ''
-    },
-    restoreItem: function(index) {
-      const tmpTodo = this.items[index];
-      tmpTodo.state = !tmpTodo.state
-      this.$set(this.items, index, tmpTodo)
-    }
-  }
+
+  // data: {
+  //     newItem: ''  
+  // },
+  // watch: {
+  //   items: function(oldValue, newValue) {
+  //     console.log('old:', JSON.stringify(oldValue))
+  //     console.log('new:', JSON.stringify(newValue))
+  //   },
+  //   deep: true,
+  //   immediate: true
+  // },
+  // methods: {
+  //   addItem: function() {
+  //     if(this.newItem === '') {
+  //       return
+  //     }
+  //     //maxIdを取得
+  //     let maxId;
+  //     this.items.forEach(function(e) {
+  //       if(maxId < e.id) {
+  //         maxId = e.id
+  //       }
+  //     })
+  //     //maxId + 1でobj作成
+  //     var newObj = {
+  //       id: maxId, todo: this.newItem, state: false
+  //     }
+  //     //itemsにセット
+  //     this.items.push(newObj)
+  //     this.newItem = ''
+  //   }
+  // }
 })
